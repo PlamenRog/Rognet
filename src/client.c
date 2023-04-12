174de378035bdff054a905946ccf99d9541db866
PlamenRog <plamen.github@gmail.com>
2023-04-12 10:55:26 +0300

    fixed the client code

--- src/client.c
+++ src/client.c
@@ -6,15 +6,14 @@
 #include <unistd.h>
 
 #define PORT 8080
-#define SERVER_ADDR "127.0.0.1" // Server IP address
+#define SERVER_ADDR "127.0.0.1"
 
 int main() {
-    char message[1024];
-    int sock;
     struct sockaddr_in server;
 
     // Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    int clientfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientfd < 0) {
         perror("Socket creation error");
         exit(EXIT_FAILURE);
     }
@@ -23,29 +22,31 @@ int main() {
     memset(&server, '0', sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
+
     if (inet_pton(AF_INET, SERVER_ADDR, &server.sin_addr) <= 0) {
         perror("Invalid address/ Address not supported");
         exit(EXIT_FAILURE);
     }
 
     // Connect to server
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    if (connect(clientfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
     // Send message to server
+    char message[1024];
     strcpy(message, "Hello from client");
-    send(sock, message, strlen(message), 0);
+    send(clientfd, message, strlen(message), 0);
 
     // Receive response from server
     memset(message, 0, sizeof(message));
-    recv(sock, message, 1024, 0);
+    recv(clientfd, message, 1024, 0);
 
     printf("Server message: %s\n", message);
 
     // Close socket
-    close(sock);
+    close(clientfd);
 
     return 0;
 }
